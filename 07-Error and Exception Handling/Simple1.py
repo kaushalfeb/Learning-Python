1f2d80c4b52ea630d9c614549418d910c38faede
Kaushal <31488893+kaushalfeb@users.noreply.github.com>
2020-01-25 12:34:14 +0530

    Using pylint for styling

---
+++ 07-Error and Exception Handling/Simple1.py
@@ -0,0 +1,11 @@
+'''
+A Simple module to use pylint.
+Uses Tabs for spacing.
+'''
+def function():
+    '''
+    A function inside Simple1 module for addition.
+    '''
+    first = 10 #First number
+    third = 20 #Second number
+    print(first+third) #Performs addition
